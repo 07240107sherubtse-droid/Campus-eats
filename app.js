de2fc4a7086d3e37e24b49b83e80d77e48a2bcea
07240107sherubtse-droid <07240107.sherubtse@rub.edu.bt>
2026-09-11 14:23:47 +0600

    Unit IV Lab 3: PostgreSQL connection, Restaurant and MenuItem models, restaurant-specific menus

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Static files (CSS, client-side JS)
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 const indexRoutes = require('./routes/index');
@@ -20,4 +21,11 @@ app.use('/', indexRoutes);
 app.listen(PORT, () => {
   console.log(`Campus Eats running at http://localhost:${PORT}`);
 });
-app.use(express.urlencoded({ extended: true }));
+
+const db = require('./config/db');
+app.get('/db-test', async (req, res) => {
+ const result = await db.one('SELECT NOW() AS current_time');
+ res.json(result);
+});
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 const aboutController = require('../controllers/aboutController');
-const menuController = require('../controllers/menuController');
-const orderController = require('../controllers/orderController');
 
 router.get('/', homeController.getHome);
 router.get('/about', aboutController.getAbout);
-router.get('/menu', menuController.getMenu);
+const menuController = require('../controllers/menuController');
+
+router.get('/restaurants/:id/menu', menuController.getMenuByRestaurant);
+
+const orderController = require('../controllers/orderController');
+
 router.post('/orders', orderController.createOrder);
 
 module.exports = router;
